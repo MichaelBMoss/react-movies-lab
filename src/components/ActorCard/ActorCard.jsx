640ee686521d32e40486352f06aedcad74194f0a
MichaelBMoss <michaelbmoss@gmail.com>
2023-12-12 21:04:27 -0800

    improve actor list css

--- src/components/ActorCard/ActorCard.jsx
+++ src/components/ActorCard/ActorCard.jsx
@@ -2,19 +2,18 @@ import './ActorCard.css';
 
 export default function ActorCard({ actor }) {
   // Generate a unique seed for each actor based on their name
-  const seed = actor.toLowerCase().replace(' ', '-');
+  const seed = actor.toLowerCase().replace(/[^a-z]+/g, '');
   // Construct the image URL with the seed
   const imageUrl = `https://picsum.photos/seed/${seed}/200/300`;
 
+  // Define the inline style with the background image
+  const cardStyle = {
+    backgroundImage: `url(${imageUrl})`,
+  };
+
   return (
-    <div className="actor-card">
-      <div className="card-data">
-        <h2>{actor}</h2>
-      </div>
-      <div>
-        <img src={imageUrl} alt={actor} className='card-img' />
-      </div>
+    <div className="actor-card" style={cardStyle}>
+      <h2>{actor}</h2>
     </div>
   );
 }
-
